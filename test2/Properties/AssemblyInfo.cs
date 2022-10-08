5d438df5ad46ccfa7a89846fc3bca764dbce59f3
Philip jensen <jogjensen@hotmail.com>
2022-10-08 22:04:45 +0200

    Fixed names

--- test2/Properties/AssemblyInfo.cs
+++ test2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("test2")]
+[assembly: AssemblyTitle("PotShow")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("test2")]
+[assembly: AssemblyProduct("PotShow")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
